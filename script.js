8994a1c9960b8cca45891e09d83ebff7efb2ebfd
dawaresatyam29-source <dawaresatyam29@gmail.com>
2026-06-22 21:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function login() {
+    let roll = document.getElementById("roll").value;
+    let phone = document.getElementById("phone").value;
+
+    alert("Roll No: " + roll + "\nPhone: " + phone);
+}
